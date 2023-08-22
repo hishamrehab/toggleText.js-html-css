817f1548cd2c047edc5609421fcd1867e9de8dc3
hisham rehab <128677920+hishamrehab@users.noreply.github.com>
2023-08-22 12:56:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeText() {
+  var x = document.getElementById("container");
+  if (x.innerHTML === "JavaScript") {
+    x.innerHTML = "Hayper Text MarkUP Language";
+  } else {
+      x.innerHTML = "JavaScript";
+  }
+}
